4ad9692fa10ab8ee5f99d285243e74d23f393854
grahamjones97 <graham.jones97@hotmail.com>
2023-12-16 15:12:04 +0000

    Commit

--- Introductory Python/Labs/Student/07-MoreOOP/clientcode.py
+++ Introductory Python/Labs/Student/07-MoreOOP/clientcode.py
@@ -5,6 +5,7 @@ from company import Programmer
 emp1 = Employee("Bill", 15000)
 emp2 = Employee("Ben", 20000)
 graham = Programmer("Graham", 30000)
+
 print(emp1.toString())
 print(emp2.toString())
 print(graham.toString())
